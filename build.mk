96a8c052283f63bc21292c32415172257327d52c
ekbduffy <ekbduffy@gmail.com>
2014-08-23 02:34:35 +0600

    Create build.mk

---
+++ build.mk
@@ -0,0 +1,20 @@
+# This file is a makefile included from the top level makefile which
+# defines the sources built for the target.
+
+# Define the prefix to this directory. 
+# Note: The name must be unique within this build and should be
+#       based on the root of the project
+TARGET_SRC_PATH = src
+
+# Add include to all objects built for this target
+INCLUDE_DIRS += inc
+
+# C source files included in this build.
+CSRC +=
+
+# C++ source files included in this build.
+CPPSRC += $(TARGET_SRC_PATH)/application.cpp
+CPPSRC += $(TARGET_SRC_PATH)/ws2812.cpp
+
+# ASM source files included in this build.
+ASRC +=
